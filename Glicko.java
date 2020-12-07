d20174253ae1502422b3eefff4c2732a9091319d
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2020-12-07 11:35:02 -0800

    Update Glicko.java
    Everything is done, I believe the math is correct

--- Glicko.java
+++ Glicko.java
@@ -1,10 +1,104 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Glicko {
-	public Glicko(ArrayList<Team> team_List) {
-		T t = new T("A",1500,350);
-		T t2 = new T("B",1700,300);
-		t.result(t2,1);//first argument is the team they played, second argument is whether t won or lost(1/0 respectively)
+	ArrayList<T> t_list = new ArrayList<>();
+	public Glicko(ArrayList<Data> data_List) {
+		//P5 stuff
+		//all P5 Conferences
+		String ACC[] = {"Boston College","Clemson","Duke","Florida State","Georgia Tech",
+				"Louisville","Miami","North Carolina","NC State","Pittsburgh","Syracuse",
+				"Virginia","Virginia Tech","Wake Forest","Notre Dame"};
+		String Big_Ten[] = {"Illinois","Indiana","Iowa","Maryland","Michigan","Michigan State",
+				"Minnesota","Nebraska","Northwestern","Ohio State","Penn State","Purdue",
+				"Rutgers","Wisconsin"};
+		String Big_Twelve[] = {"Baylor","Iowa State","Kansas","Kansas State","Oklahoma",
+				"Oklahoma State","TCU","Texas","Texas Tech","West Virginia"};
+		String Pac_Twelve[] = {"Arizona","Arizona State","California","UCLA","Colorado",
+				"Oregon","Oregon State","USC","Stanford","Utah","Washington",
+				"Washington State"};
+		String SEC[] = {"Alabama","Arkansas","Auburn","Florida","Georgia","Kentcuky",
+				"LSU","Ole Miss","Mississippi State","Missouri","South Carolina",
+				"Tennessee","Texas A&M","Vanderbilt"};
+		String[] all_P5 = new String[ACC.length + Big_Ten.length + Big_Twelve.length + Pac_Twelve.length + SEC.length];
+		System.arraycopy(ACC, 0, all_P5, 0, ACC.length);
+		System.arraycopy(Big_Ten, 0, all_P5, ACC.length, Big_Ten.length);
+		System.arraycopy(Big_Twelve, 0, all_P5, ACC.length+Big_Ten.length, Big_Twelve.length);
+		System.arraycopy(Pac_Twelve, 0, all_P5, ACC.length+Big_Ten.length+Big_Twelve.length, Pac_Twelve.length);
+		System.arraycopy(SEC, 0, all_P5, ACC.length+Big_Ten.length+Big_Twelve.length+Pac_Twelve.length, SEC.length);
+		//
+		for(int i = 0;i < data_List.size();i++){
+			if(t_list.size() == 0){
+				T t1 = null, t2 = null;
+				if(Arrays.asList(all_P5).contains(data_List.get(i).team1)){
+					t1 = new T(data_List.get(i).team1,1750,350);					
+				}
+				else{
+					t1 = new T(data_List.get(i).team1,1500,350);					
+				}
+				if(Arrays.asList(all_P5).contains(data_List.get(i).team2)){
+					t2 = new T(data_List.get(i).team2,1750,350);					
+				}
+				else{
+					t2 = new T(data_List.get(i).team2,1500,350);					
+				}
+				//Team 1 won
+				if(data_List.get(i).winner.equals(data_List.get(i).team1)){
+					t1.result(t2, 1);
+					t2.result(t1, 0);
+				}
+				else{
+					t1.result(t2, 0);
+					t2.result(t1, 1);
+				}
+				t_list.add(t1);
+				t_list.add(t2);
+			}
+			else{
+				T t_1 = null, t_2 = null;
+				boolean found_t1 = false, found_t2 = false;
+				for(int a = 0;a < t_list.size();a++){
+					if(t_list.get(a).name.equals(data_List.get(i).team1)){
+						t_1 = t_list.get(a);
+						found_t1 = true;
+					}
+					if(t_list.get(a).name.equals(data_List.get(i).team2)){
+						t_2 = t_list.get(a);
+						found_t2 = true;
+					}
+				}
+				if(found_t1 == false){
+					if(Arrays.asList(all_P5).contains(data_List.get(i).team1)){
+						t_1 = new T(data_List.get(i).team1,1750,350);					
+					}
+					else{
+						t_1 = new T(data_List.get(i).team1,1500,350);					
+					}
+					t_list.add(t_1);
+				}
+			    if(found_t2 == false){
+					if(Arrays.asList(all_P5).contains(data_List.get(i).team2)){
+						t_2 = new T(data_List.get(i).team2,1750,350);					
+					}
+					else{
+						t_2 = new T(data_List.get(i).team2,1500,350);					
+					}
+			    	t_list.add(t_2);
+			    }
+				if(data_List.get(i).winner.equals(data_List.get(i).team1)){
+					t_1.result(t_2, 1);
+					t_2.result(t_1, 0);
+				}
+				else{
+					t_1.result(t_2, 0);
+					t_2.result(t_1, 1);
+				}
+			}
+		}
+		//printing results
+		for(int i = 0;i < t_list.size();i++){
+			System.out.println(t_list.get(i).name + "," + t_list.get(i).rating + "," + t_list.get(i).RD);
+		}
 	}
 
 }
@@ -45,11 +139,11 @@ class T{
 			B = Math.log(Math.pow(delta, 2) - Math.pow(n_RD, 2) - v);
 		}
 		else{
-			Iteration(delta,v,a,1);
+			Iteration(delta,v,a,1.0);
 			B = BB;
 		}
 		//Step 3 and 4 of the Iterative process
-		double e = .000001;
+		double e = .0000001;//5 zeroes but try messing around with this
 		double C;
 		int div_a = 1;
 		while(Math.abs((B-A)) > e){
@@ -65,6 +159,7 @@ class T{
 				break;
 			}
 		}
+		vol = Math.exp(A/2);
 		//Step 6 overall
 		double RD_apo = Math.sqrt(Math.pow(n_RD,2)+Math.pow(Math.exp(A/2),2));
 		//Step 7 overall
@@ -72,11 +167,11 @@ class T{
 		n_r = n_r + Math.pow(n_RD, 2)*(g_omega * (res - E));
 		rating = 173.7178*n_r + 1500;
 		RD = 173.7178*n_RD;
-		System.out.println(rating + " , " + RD);
+		
 	}
 	private void Iteration(double delta,double v, double a, double K){
 		if(main_func(a-(K*system_constant),delta,v,a) < 0){
-			Iteration(delta,v,a,K+1);
+			Iteration(delta,v,a,K+1.0);
 		}
 		else{
 			BB = (a - (K*system_constant));
@@ -91,6 +186,8 @@ class T{
 		//second fraction
 		double s_frac = (x - a) / Math.pow(system_constant, 2);
 		//subtract the fraction
-		return ret = f_frac - s_frac;
+		//System.out.println(f_frac - s_frac);
+		ret = f_frac - s_frac;
+		return ret;
 	}
 }
