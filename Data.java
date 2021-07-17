ff0096a13a7f08d6ce695b6afdbabf44157334da
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-07-16 17:34:36 -0700

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,38 +1,21 @@
 
 public class Data {
-    String[] fullData;
-    int t1_index , t2_index;
-    int p1_index , p2_index;
-    String team1 , team2;
-    int point1 , point2;
-    String winner = "" , loser = "";
-    boolean team1winner = false;
-    boolean delete = false;
-    public Data(String[] arr,int team1_index,int point1_index,int team2_index,int point2_index){
-        fullData = arr;
-        t1_index = team1_index;
-        t2_index = team2_index;
-        p1_index = point1_index;
-        p2_index = point2_index;
-        if(fullData[p1_index].equals("") || fullData[p2_index].equals("")){
-            delete = true;
-        }
-        else{
-            //correctly finding the data
-            team1 = fullData[team1_index];
-            team2 = fullData[team2_index];
-            point1 = Integer.parseInt(fullData[p1_index]);
-            point2 = Integer.parseInt(fullData[p2_index]);
-            //finding winner and loser
-            if(point1 > point2 ){
-                winner = team1;
-                loser = team2;
-                team1winner = true;
-            }
-            else if(point1 < point2){
-                winner = team2;
-                loser = team1;
-            }
-        }
-    }
+	String homeT,awayT;
+	int homeP,awayP;
+	String winner, loser;
+	public Data(String ht,String hp,String at,String ap) {
+		homeT = ht;
+		awayT = at;
+		homeP = Integer.parseInt(hp);
+		awayP = Integer.parseInt(ap);
+		//Interpret Winner/Loser
+		if(homeP > awayP) {
+			winner = homeT;
+			loser = awayT;
+		}
+		else {
+			winner = awayT;
+			loser = homeT;
+		}
+	}
 }
