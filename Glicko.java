c9b0953a4fcf86c45f9078cf763559d892b80d9c
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-07-16 17:35:08 -0700

    Update Glicko.java

--- Glicko.java
+++ Glicko.java
@@ -1,119 +1,51 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Glicko {
+	ArrayList<Data> dl;
+	ArrayList<Team> tl;
+	
 	ArrayList<T> t_list = new ArrayList<>();
-	public Glicko(ArrayList<Data> data_List) {
-		//P5 stuff
-		//all P5 Conferences
-		String ACC[] = {"Boston College","Clemson","Duke","Florida State","Georgia Tech",
-				"Louisville","Miami","North Carolina","NC State","Pittsburgh","Syracuse",
-				"Virginia","Virginia Tech","Wake Forest","Notre Dame"};
-		String Big_Ten[] = {"Illinois","Indiana","Iowa","Maryland","Michigan","Michigan State",
-				"Minnesota","Nebraska","Northwestern","Ohio State","Penn State","Purdue",
-				"Rutgers","Wisconsin"};
-		String Big_Twelve[] = {"Baylor","Iowa State","Kansas","Kansas State","Oklahoma",
-				"Oklahoma State","TCU","Texas","Texas Tech","West Virginia"};
-		String Pac_Twelve[] = {"Arizona","Arizona State","California","UCLA","Colorado",
-				"Oregon","Oregon State","USC","Stanford","Utah","Washington",
-				"Washington State"};
-		String SEC[] = {"Alabama","Arkansas","Auburn","Florida","Georgia","Kentcuky",
-				"LSU","Ole Miss","Mississippi State","Missouri","South Carolina",
-				"Tennessee","Texas A&M","Vanderbilt"};
-		String[] all_P5 = new String[ACC.length + Big_Ten.length + Big_Twelve.length + Pac_Twelve.length + SEC.length];
-		System.arraycopy(ACC, 0, all_P5, 0, ACC.length);
-		System.arraycopy(Big_Ten, 0, all_P5, ACC.length, Big_Ten.length);
-		System.arraycopy(Big_Twelve, 0, all_P5, ACC.length+Big_Ten.length, Big_Twelve.length);
-		System.arraycopy(Pac_Twelve, 0, all_P5, ACC.length+Big_Ten.length+Big_Twelve.length, Pac_Twelve.length);
-		System.arraycopy(SEC, 0, all_P5, ACC.length+Big_Ten.length+Big_Twelve.length+Pac_Twelve.length, SEC.length);
-		//
-		for(int i = 0;i < data_List.size();i++){
+	public Glicko(ArrayList<Data> game_List, ArrayList<Team> team_List) {
+		dl = game_List;
+		tl = team_List;
+		setupData();
+	}
+	
+	private void setupData(){
+		for(int i = 0;i < dl.size();i++) {
 			if(t_list.size() == 0){
-				T t1 = null, t2 = null;
-				if(Arrays.asList(all_P5).contains(data_List.get(i).team1)){
-					t1 = new T(data_List.get(i).team1,1750,350);					
-				}
-				else{
-					t1 = new T(data_List.get(i).team1,1500,350);					
-				}
-				if(Arrays.asList(all_P5).contains(data_List.get(i).team2)){
-					t2 = new T(data_List.get(i).team2,1750,350);					
-				}
-				else{
-					t2 = new T(data_List.get(i).team2,1500,350);					
-				}
-				//Team 1 won
-				if(data_List.get(i).winner.equals(data_List.get(i).team1)){
-					t1.result(t2, 1);
-					t2.result(t1, 0);
-				}
-				else{
-					t1.result(t2, 0);
-					t2.result(t1, 1);
-				}
-				t1.opp_list.add(t2);
-				t2.opp_list.add(t1);
+				T t1 = new T(dl.get(i).winner,1500,350);
+				T t2 = new T(dl.get(i).loser,1500,350);
+				t1.result(t2,1);
+				t2.result(t1,0);
 				t_list.add(t1);
 				t_list.add(t2);
 			}
-			else{
-				T t_1 = null, t_2 = null;
-				boolean found_t1 = false, found_t2 = false;
-				for(int a = 0;a < t_list.size();a++){
-					if(t_list.get(a).name.equals(data_List.get(i).team1)){
-						t_1 = t_list.get(a);
-						found_t1 = true;
+			else {
+				T t1 = new T(dl.get(i).winner,1500,350);
+				T t2 = new T(dl.get(i).loser,1500,350);
+				boolean one_found = false,two_found = false;
+				for(int a = 0;a < t_list.size();a++) {
+					if(t_list.get(a).name.equals(t1.name)) {
+						t1 = t_list.get(a);
+						one_found = true;
 					}
-					if(t_list.get(a).name.equals(data_List.get(i).team2)){
-						t_2 = t_list.get(a);
-						found_t2 = true;
+					if(t_list.get(a).name.equals(t2.name)) {
+						t2 = t_list.get(a);
+						two_found = true;
 					}
 				}
-				if(found_t1 == false){
-					if(Arrays.asList(all_P5).contains(data_List.get(i).team1)){
-						t_1 = new T(data_List.get(i).team1,1750,350);					
-					}
-					else{
-						t_1 = new T(data_List.get(i).team1,1500,350);					
-					}
-					t_list.add(t_1);
+				t1.result(t2, 1);
+				t2.result(t1, 0);
+				if(one_found == false) {
+					t_list.add(t1);
 				}
-			    if(found_t2 == false){
-					if(Arrays.asList(all_P5).contains(data_List.get(i).team2)){
-						t_2 = new T(data_List.get(i).team2,1750,350);					
-					}
-					else{
-						t_2 = new T(data_List.get(i).team2,1500,350);					
-					}
-			    	t_list.add(t_2);
-			    }
-				if(data_List.get(i).winner.equals(data_List.get(i).team1)){
-					t_1.result(t_2, 1);
-					t_2.result(t_1, 0);
+				if(two_found == false) {
+					t_list.add(t2);
 				}
-				else{
-					t_1.result(t_2, 0);
-					t_2.result(t_1, 1);
-				}
-				t_1.opp_list.add(t_2);
-				t_2.opp_list.add(t_1);
 			}
-		}
-		//Calculating Strength of Schedule
-		for(int i = 0;i < t_list.size();i++){
-			double total_e = 0.0;
-			for(int a = 0;a < t_list.get(i).opp_list.size();a++){
-				total_e += t_list.get(i).opp_list.get(a).rating;
-			}
-			t_list.get(i).SOS = (total_e / (double)t_list.get(i).opp_list.size());
-		}
-		//printing results
-		System.out.println("NAME,RATING,RATING DEVIATION,STRENGTH OF SCHEDULE");
-		for(int i = 0;i < t_list.size();i++){
-			System.out.println(t_list.get(i).name + "," + t_list.get(i).rating + "," + t_list.get(i).RD + "," + t_list.get(i).SOS);
 		}
 	}
-
 }
 class T{
 	double system_constant = .5;//between .3 and 1.2
@@ -130,13 +62,14 @@ class T{
 	double n_RD;
 	//d
 	double BB = 0.0;
-	public T(String n,double r,double rd){
+	public T(String n, double r, double rd) {
 		name = n;
 		rating = r;
 		RD = rd;
 		n_r = (rating-1500)/173.7178;
 		n_RD = RD/173.7178;
 	}
+	
 	public void result(T t2,int res) {
 		//Step 3
 		//computing the estimtated variance of the players ratings based only on game outcomes
@@ -182,8 +115,7 @@ class T{
 		n_RD = 1 / Math.sqrt(1/Math.pow(RD_apo, 2) + 1/v);
 		n_r = n_r + Math.pow(n_RD, 2)*(g_omega * (res - E));
 		rating = 173.7178*n_r + 1500;
-		RD = 173.7178*n_RD;
-		
+		RD = 173.7178*n_RD;		
 	}
 	private void Iteration(double delta,double v, double a, double K){
 		if(main_func(a-(K*system_constant),delta,v,a) < 0){
