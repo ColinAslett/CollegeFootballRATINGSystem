e0a688284b2092eae7162221ea09249855a58549
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-08-16 20:38:02 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -12,13 +14,14 @@ public class Main {
 	PageRank pageR;
 	HITS hits;
 	public Main() throws Exception{
-		read_csv("/Users/colinaslett/Desktop/CFB_2020_REG_DATA.csv");
+		read_csv("/Users/colinaslett/Desktop/CFB_2019_REG_DATA.csv");
 		create_team_list();
-		//elo = new ELO(Game_List,Team_List);
+		System.out.println("VICTOR,P5_DIFF,H_W_M_A_W,H_L_M_A_L,H_ELO_M_A_ELO,WEEK");
+		elo = new ELO(Game_List,Team_List);
 		//glicko = new Glicko(Game_List,Team_List);
 		//beatG = new BeatGraph(Game_List,Team_List);
 		//pageR = new PageRank(Game_List,Team_List);
-		hits = new HITS(Game_List,Team_List);
+		//hits = new HITS(Game_List,Team_List);
 		//printELO();
 		//printGlicko();
 		//printBeatGraph();
@@ -37,9 +40,9 @@ public class Main {
 				String[] temp_array = temp.split(",",0);
 				//HT = 12, HP = 14, AT = 21, AP = 23
 				//Week Filter, this is useful only when doing rating system performance
-				//if(Integer.parseInt(temp_array[2]) <= 2) {
-					Game_List.add(new Data(temp_array[12],temp_array[14],temp_array[21],temp_array[23]));
-				//}
+				if(Integer.parseInt(temp_array[2]) >= 1) {
+					Game_List.add(new Data(temp_array[12],temp_array[14],temp_array[21],temp_array[23],temp_array[2]));
+				}
 			}
 			count++;
 			temp = sc.readLine();
@@ -72,14 +75,21 @@ public class Main {
 		//Sort it, highest->lowest
 		for(int i = 0;i < Team_List.size();i++) {
 			for(int a = 0;a < Team_List.size();a++) {
-				if(Team_List.get(i).ELO > Team_List.get(a).ELO) {
+				//if(Team_List.get(i).ELO > Team_List.get(a).ELO) {
+					//Collections.swap(Team_List, i, a);
+				//}
+				//Sorted Alphabetically
+				if((int)Team_List.get(i).name.charAt(0) < (int)Team_List.get(a).name.charAt(0)) {
 					Collections.swap(Team_List, i, a);
 				}
 			}
 		}
 		for(int i = 0;i < Team_List.size();i++) {
 			//System.out.println(Team_List.get(i).name + "," + Team_List.get(i).ELO);
-			System.out.println(Team_List.get(i).ELO);
+			BigDecimal bd = new BigDecimal(Team_List.get(i).ELO).setScale(2,RoundingMode.HALF_UP);
+			double r = bd.doubleValue();
+			System.out.println(Team_List.get(i).name);
+			System.out.println(r);
 		}
 	}
 	//Prinotout of Glicko rating system
@@ -93,7 +103,13 @@ public class Main {
 			}
 		}
 		for(int i = 0;i < glicko.t_list.size();i++){
-			System.out.println(glicko.t_list.get(i).name + "," + glicko.t_list.get(i).rating + "," + glicko.t_list.get(i).RD);
+			BigDecimal bd = new BigDecimal(glicko.t_list.get(i).rating).setScale(2,RoundingMode.HALF_UP);
+			BigDecimal bd2 = new BigDecimal(glicko.t_list.get(i).RD).setScale(2,RoundingMode.HALF_UP);
+			double avg = bd.doubleValue();
+			double rd = bd2.doubleValue();
+			//System.out.println(avg + " , " + rd);
+			System.out.println(glicko.t_list.get(i).name);
+			//System.out.println(glicko.t_list.get(i).name + "," + glicko.t_list.get(i).rating + "," + glicko.t_list.get(i).RD);
 		}
 	}
 	//Printout of the BeatGraph Rating System
@@ -109,8 +125,10 @@ public class Main {
 			}
 		}
 		for(int i = 0;i < beatG.result_list.size();i++) {
-			System.out.println(beatG.result_list.get(i).name + "," + (beatG.result_list.get(i).beatWins-beatG.result_list.get(i).beatLosses)
-					+ "," + beatG.result_list.get(i).beatWins + "," + beatG.result_list.get(i).beatLosses);
+			//System.out.println(beatG.result_list.get(i).name);
+			System.out.println(beatG.result_list.get(i).beatWins + "-" + beatG.result_list.get(i).beatLosses);
+			//System.out.println(beatG.result_list.get(i).name + "," + (beatG.result_list.get(i).beatWins-beatG.result_list.get(i).beatLosses)
+					//+ "," + beatG.result_list.get(i).beatWins + "," + beatG.result_list.get(i).beatLosses);
 		}
 	}
 	//Printout of the PageRank Rating System
@@ -124,7 +142,11 @@ public class Main {
 			}
 		}
 		for(int i = 0;i < pageR.res_list.size();i++) {
-			System.out.println(pageR.res_list.get(i).name + "," + pageR.res_list.get(i).score*10000);
+			//System.out.println(pageR.res_list.get(i).name);
+			BigDecimal bd = new BigDecimal(pageR.res_list.get(i).score*10000).setScale(2,RoundingMode.HALF_UP);
+			double r = bd.doubleValue();
+			System.out.println(r);
+			//System.out.println(pageR.res_list.get(i).name + "," + pageR.res_list.get(i).score*10000);
 		}
 	}
 }
